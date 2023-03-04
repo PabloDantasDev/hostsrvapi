3a4c0e1ab8d79dad1b8037130fb259cd0049f1d6
Pablo Henrique Dantas <83853353+PabloDantasDev@users.noreply.github.com>
2023-03-04 11:53:05 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,18 +1,12 @@
-import express from 'express';
-import { Router } from 'express';
-import cors from 'cors';
-import axios from 'axios';
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
-const route = Router();
 
-app.use(cors({
-  origin: '*',
-  optionsSuccessStatus: 200 // Alguns navegadores (por exemplo, IE11) não suportam o status[r.monarge] 204
-}));
+app.use(cors());
 
-
-route.get('/cpf/:cpf', async (req, res) => {
+app.get('/api/cpf/:cpf', async (req, res) => {
   try {
     const cpf = req.params.cpf;
     const response = await axios.get(`https://isyubii-api.tk/puxar?type=cpf3&q=${cpf}&token=Eltonpainel`);
@@ -23,12 +17,4 @@ route.get('/cpf/:cpf', async (req, res) => {
   }
 });
 
-
-
-app.use(route);
-
-const port = process.env.PORT || 4000;
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+module.exports = app;
